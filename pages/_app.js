02c1e0ee91b6e0af9aca684e040a14f6edadb961
sachin-source <sachinmgvt@gmail.com>
2022-10-26 13:22:44 +0530

    login page basic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,52 @@
-import '../styles/globals.css'
+import { useRouter } from 'next/router';
+import { userService } from '../services';
 
+import { useState, useEffect } from 'react';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [authorized, setAuthorized] = useState(false);
+
+  function authCheck(url) {
+    // redirect to login page if accessing a private page and not logged in 
+    setUser(userService.userValue);
+    const publicPaths = ['/account/login', '/account/register'];
+    const path = url.split('?')[0];
+    if (!userService.userValue && !publicPaths.includes(path)) {
+      setAuthorized(false);
+      router.push({
+        pathname: '/account/login',
+        query: { returnUrl: router.asPath }
+      });
+    } else {
+      setAuthorized(true);
+    }
+  }
+
+  useEffect(() => {
+    authCheck(router.asPath);
+
+        // on route change start - hide page content by setting authorized to false  
+        const hideContent = () => setAuthorized(false);
+        router.events.on('routeChangeStart', hideContent);
+
+        // on route change complete - run auth check 
+        router.events.on('routeChangeComplete', authCheck)
+
+        // unsubscribe from events in useEffect return function
+        return () => {
+            router.events.off('routeChangeStart', hideContent);
+            router.events.off('routeChangeComplete', authCheck);
+        }
+  }, [])
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <p>comman tag</p>
+    </>
+  )
 }
 
 export default MyApp
