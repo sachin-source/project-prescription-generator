0e4e51dedb399acb020519f54935253201239b99
sachin-source <sachinmgvt@gmail.com>
2022-12-27 21:17:29 +0530

    adding the first page

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,17 @@ export default function Home() {
         <title>Login Page</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <main>
-        <div>main page</div>
+      <main className='patient-details-container' >
+        <div className="patient-details">
+          <div className="patient-detail-field full-screen">
+            <label htmlFor="name">Name :</label>
+            <input type="text" id="name" name='name' />
+          </div>
+          <div className="patient-detail-field half-screen">
+            <label htmlFor="phone">phone :</label>
+            <input type="text" id="phone" name='phone' />
+            </div>
+        </div>
       </main>
     </div>
   )
