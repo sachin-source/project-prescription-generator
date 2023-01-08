45d48fc3565aa2b953a63f67cde9c293a3ac28e7
sachin-source <sachinmgvt@gmail.com>
2023-01-08 20:49:47 +0530

    started with patient details part

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,50 @@ export default function Home() {
         <title>Login Page</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
+
+{/* 
+Patient details
+	1. Patient name
+	2. Age
+	3. Disease / Symptoms ?
+	4. Phone number
+	5. Appointment details
+	6. Appointment date
+	7. 
+*/}
+
       <main className='patient-details-container' >
+        <div className="heading-container">
+          <h2>Patient details</h2>
+        </div>
         <div className="patient-details">
-          <div className="patient-detail-field full-screen">
-            <label htmlFor="name">Name :</label>
-            <input type="text" id="name" name='name' />
+          <div className="patient-name-container">
+            <label htmlFor="patient-name" className="patient-name-label">patient name</label>
+            <input type="text" name="patient-name" id="patient-name" className="patient-name" />
           </div>
-          <div className="patient-detail-field half-screen">
-            <label htmlFor="phone">phone :</label>
-            <input type="text" id="phone" name='phone' />
-            </div>
+          <div className="patient-age-container">
+            <label htmlFor="patient-age" className="patient-age-label">patient age</label>
+            <input type="number" name="patient-age" id="patient-age" className="patient-age" />
+          </div>
+          <div className="patient-disease-container">
+            <label htmlFor="patient-disease" className="patient-disease-label">disease</label>
+            <input type="text" name="patient-disease" id="patient-disease" className="patient-disease" />
+          </div>
+          <div className="patient-contact-number-container">
+            <label htmlFor="patient-contact-number" className="patient-contact-number-label">contact number</label>
+            <input type="number" name="patient-contact-number" id="patient-contact-number" className="patient-contact-number" />
+          </div>
+          <div className="patient-appointment-details-container">
+            <label htmlFor="patient-appointment-details" className="patient-appointment-details-label">appointment details</label>
+            <input type="text" name="patient-appointment-details" id="patient-appointment-details" className="patient-appointment-details" />
+          </div>
+          <div className="patient-appointment-date-container">
+            <label htmlFor="patient-appointment-date" className="patient-appointment-date-label">appointment date</label>
+            <input type="date" name="patient-appointment-date" id="patient-appointment-date" className="patient-appointment-date" />
+          </div>
+          <div className="patient-details-submit">
+            <button>submit</button>
+          </div>
         </div>
       </main>
     </div>
