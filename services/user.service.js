4ba24fa2b9c7a56c478d3ec9f37740d345a62d57
sachin-source <sachinmgvt@gmail.com>
2023-01-26 12:11:56 +0530

    netlify deploy try

--- services/user.service.js
+++ services/user.service.js
@@ -5,7 +5,7 @@ import Router from 'next/router';
 // import { fetchWrapper } from 'helpers';
 
 const { publicRuntimeConfig } = getConfig();
-const baseUrl = `${publicRuntimeConfig.apiUrl}/users`;
+// const baseUrl = `${publicRuntimeConfig.apiUrl}/users`;
 const userSubject = new BehaviorSubject(process.browser && JSON.parse(localStorage.getItem('user')));
 
 export const userService = {
