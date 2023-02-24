372aa9604dca2db2eb22f4ed48a8df7a96d51ab6
sachin-source <sachinmgvt@gmail.com>
2023-02-24 11:03:29 +0530

    navbar customization based on login

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import styles from "../styles/Navbar.module.css"
 
 const APP_TITLE = "ERX"
-function Navbar() {
+function Navbar({ authorized }) {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  useEffect(() => {
+    setisLoggedIn(localStorage.getItem('user'))
+  }, [])
+  
   return (
     <div className={styles["navbar-container"]}>
         <div className={styles["logo-container"]}>
@@ -12,12 +17,15 @@ function Navbar() {
             ))}</h2>
             {/* <span className="navbar-logo-description">Tech-scription</span> */}
         </div>
-        <div className={styles["navbar-elements-container"]}>
-            <div className={styles["navbar-element"]}>new</div>
-            <div className={styles["navbar-element"]}>patients</div>
-            <div className={styles["navbar-element"]}>prescriptions</div>
-            <div className={styles["navbar-element"]}>archieves</div>
-        </div>
+        {
+          isLoggedIn ? (<div className={styles["navbar-elements-container"]}>
+          <div className={styles["navbar-element"]}>new</div>
+          <div className={styles["navbar-element"]}>patients</div>
+          <div className={styles["navbar-element"]}>prescriptions</div>
+          <div className={styles["navbar-element"]}>archieves</div>
+      </div>) : (<></>)
+        }
+        
     </div>
   )
 }
