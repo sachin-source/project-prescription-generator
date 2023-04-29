9ccf777461996648c8b8544b4ea7c1a36ac40bb8
sachin-source <sachinmgvt@gmail.com>
2023-04-29 17:50:14 +0530

    service issue fix

--- services/index.js
+++ services/index.js
@@ -1,6 +1,2 @@
-// export * from './user.service';
-// export * from './http.service';
-
-import userService from './user.service';
-import httpService from './http.service';
-export { userService, httpService }
+export * from './user.service';
+export * from './http.service';
