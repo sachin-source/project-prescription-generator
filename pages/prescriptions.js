20616d9abb57731c085028e7e9cfe9b358f14464
sachin-source <sachinmgvt@gmail.com>
2023-04-30 17:23:38 +0530

    started with prescription upload

--- pages/prescriptions.js
+++ pages/prescriptions.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
-const APP_TITLE = "ERX"
 export default function Prescriptions() {
 
-
-
-
 return (
-    <div  className='inprogress'>
-      <h2>This page is in progress</h2>
+    // <div  className='inprogress'>
+    //   <h2>This page is in progress</h2>
+    // </div>
+    <div className='prescription-upload-container' >
+      <label htmlFor='prescription-file' >upload the excel file here</label>
+      <input type='file' id='prescription-file' name='prescription-file' hidden />
     </div>
 )
 
