858bbf18b3c905f812ca2a8efcfab17d349fd7ed
sachin-source <sachinmgvt@gmail.com>
2023-05-03 17:18:25 +0530

    started with prescription upload

--- pages/prescriptions.js
+++ pages/prescriptions.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Prescriptions() {
+  const [file, setFile] = useState(undefined);
 
-return (
-    // <div  className='inprogress'>
-    //   <h2>This page is in progress</h2>
-    // </div>
+  const upload  = () => {
+    const data = new FormData();
+    data.append("file", file);
+    fetch("http://localhost:3005/prescription/upload", { method: "POST", body: data, headers : { token : localStorage.getItem("token") } })
+    .then((response) => {
+      console.log(response)
+    })
+  }
+
+  const onChange  = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  return (
+      // <div  className='inprogress'>
+      //   <h2>This page is in progress</h2>
+      // </div>
     <div className='prescription-upload-container' >
       <label htmlFor='prescription-file' >upload the excel file here</label>
-      <input type='file' id='prescription-file' name='prescription-file' hidden />
+      <input type='file' id='prescription-file' name='prescription-file' onChange={onChange} hidden/>
+      <button onClick={upload} >upload</button>
     </div>
-)
-
+  )
 }
 
