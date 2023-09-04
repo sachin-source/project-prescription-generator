8b75b67bdd42a5280aa4e6c07c53de347f7ff19f
sachin-source <sachinmgvt@gmail.com>
2023-09-05 00:31:50 +0530

    production setup

--- services/http.service.js
+++ services/http.service.js
@@ -2,7 +2,7 @@ export const httpService = {
     getPatientList
 };
 
-const apiEndPoint = "http://localhost:3005/";
+const apiEndPoint =  "https://prescription-backend.onrender.com/" // "http://localhost:3005/";
 
 function getPatientList() {
     return fetch(apiEndPoint + 'patient').then(res => res.json())
